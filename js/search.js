8941c6395e7055514fab1f5ea4609b30a26f02c2
최규형 <hyounghe0724@sungkyul.ac.kr>
2023-03-29 05:34:43 +0000

    23년 3월 29일 자바 웹 프로그래밍 4주차 실습 완료

---
+++ js/search.js
@@ -0,0 +1,14 @@
+const search_btn = document.getElementById("search_btn");
+
+/*
+function search_message(){
+	alert("검색을 수행합니다!");
+}
+*/
+
+const search_message = () =>{
+	alert("검색을 수행합니다!");
+}
+
+search_btn.addEventListener("click" , search_message);
+
