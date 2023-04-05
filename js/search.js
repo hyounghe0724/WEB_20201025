412d2339e8434fa7a8a21723872064e467a76fd2
최규형 <hyounghe0724@sungkyul.ac.kr>
2023-04-05 05:32:12 +0000

    자바웹프로그래밍 5주차 실습완료

--- js/search.js
+++ js/search.js
@@ -1,5 +1,6 @@
 const search_btn = document.getElementById("search_btn");
 
+
 /*
 function search_message(){
 	alert("검색을 수행합니다!");
@@ -8,7 +9,9 @@ function search_message(){
 
 const search_message = () =>{
 	alert("검색을 수행합니다!");
+	let search_str = document.querySelector("#search_txt"); // 변수에 저장
+    document.getElementById("search_message").innerHTML = search_str.value; // 태그에 값 추가
+    console.log(search_str.value); // 콘솔에 출력
 }
 
 search_btn.addEventListener("click" , search_message);
-
